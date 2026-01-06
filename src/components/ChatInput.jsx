d8c2e0c765dd287f1de21623b3a6cd5d3c4c637c
aboulayetchakoura <aboulayet63@gmail.com>
2026-01-06 13:05:12 -0500

    refracture the components

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,11 @@
+export const ChatInput = () => {
+  return (
+    <>
+    <h2>hello from chat input</h2>
+    <div> 
+        <input type="text"  placeholder="How may i help you?" />
+        <button>Send</button>
+    </div>
+    </>
+  )
+}
